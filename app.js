ae8c36b4dbade92f75cb71b6134404f6ff9e97c9
Aditya Bhanushali <bhanushaliaditya60@gmail.com>
2023-08-01 20:33:52 +0530

    created swipe project

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburgerbutton = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+const bar1 = document.querySelector('.bar1');
+const bar2 = document.querySelector('.bar2');
+const bar3 = document.querySelector('.bar3');
+hamburgerbutton.addEventListener('click', () => {
+  bar1.classList.toggle('animateBar1');
+  bar2.classList.toggle('animateBar2');
+  bar3.classList.toggle('animateBar3');
+  mobileNav.classList.toggle('openDrawer');
+  bar1.classList.toggle('dark');
+  bar3.classList.toggle('dark');
+});
